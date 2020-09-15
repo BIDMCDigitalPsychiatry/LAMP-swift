e1cf2f6d0a88291b3c4b331c1aa1ac1444dd3a0c
Jijo Pulikkottil <>
2020-09-15 12:22:18 +0530

    New branch for Sensor package

--- Package.swift
+++ Package.swift
@@ -1,25 +1,29 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "LAMP",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_11),
-        .tvOS(.v9),
-        .watchOS(.v3)
-    ],
+    name: "Sensors",
+    platforms: [.iOS(.v12), .watchOS(.v6)],
     products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "LAMP",
-            targets: ["LAMP"]),
+            name: "Sensors",
+            targets: ["Sensors"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "LAMP",
-            dependencies: [],
-            path: "LAMP/Classes"
-        ),
+            name: "Sensors",
+            dependencies: []),
+        .testTarget(
+            name: "SensorsTests",
+            dependencies: ["Sensors"]),
     ]
 )
