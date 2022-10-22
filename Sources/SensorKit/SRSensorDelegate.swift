a11809b782edb1c039ecd2a6b7c9871a35c24da2
jijopulikkottil <jijo.pulikkottil@zcocorporation.com>
2022-10-22 12:59:47 +0530

    created custom models to to encode sensorkit data

--- Sources/SensorKit/SRSensorDelegate.swift
+++ Sources/SensorKit/SRSensorDelegate.swift
@@ -10,17 +10,52 @@ import Foundation
 import SensorKit
 import Speech
 
+extension Encodable {
+  var convertToDict: [String: Any]? {
+    guard let data = try? JSONEncoder().encode(self) else { return nil }
+    return (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
+  }
+}
+
 class SRSensorDelegate: NSObject, SRSensorReaderDelegate {
     
     weak var callbackdelegate: SensorKitObserver?
     
     var frequencySettings: [String: SRSensorLoader.Config]?
     
+//    private func processResult(_ result: SRFetchResult<AnyObject>, sensor: SRSensor) {
+//
+//        guard let sample = result.sample as? Encodable, let json = sample.convertToDict else { return }
+//        let date = NSDate(srAbsoluteTime: result.timestamp)
+//        let model =  SensorKitEvent(timestamp: date.timeIntervalSince1970 * 1000, sensor: sensor.lampIdentifier, data: json)
+//        callbackdelegate?.onSensorFetch(model)
+//    }
+    
     private func processResult(_ result: SRFetchResult<AnyObject>, sensor: SRSensor) {
-        
-        guard let sample = result.sample as? Encodable, let json = sample.convertToDict else { return }
+
+        var sample: Encodable?
+        if let wristDetection = result.sample as? SRWristDetection {
+            sample = SRWristDetectionCustom(wristDetection)
+        } else if let visit = result.sample as? SRVisit {
+            sample = SRVisitCustom(visit)
+        } else if let keyboardMetrics = result.sample as? SRKeyboardMetrics {
+            sample = SRKeyboardMetricsCustom(keyboardMetrics)
+        } else if let messageUsage = result.sample as? SRMessagesUsageReport {
+            sample = SRMessagesUsageReportCustom(messageUsage)
+        } else if let deviceUsage = result.sample as? SRDeviceUsageReport {
+            sample = SRDeviceUsageReportCustom(deviceUsage)
+        } else if let phoneUsage = result.sample as? SRPhoneUsageReport {
+            sample = SRPhoneUsageReportCustom(phoneUsage)
+        } else if let lightSample = result.sample as? SRAmbientLightSample {
+            sample = SRAmbientLightSampleCustom(lightSample)
+        } else if let speechRecognitionResult = result.sample as? SFSpeechRecognitionResult {
+            if #available(iOS 15.0, *) {
+                sample = SFSpeechRecognitionResultCustom(speechRecognitionResult)
+            }
+        }
+        guard let json = sample?.convertToDict else { return }
         let date = NSDate(srAbsoluteTime: result.timestamp)
-        let model =  SensorKitEvent(timestamp: date.timeIntervalSince1970 * 1000, sensor: sensor.lampIdentifier, data: json)
+        let model =  SensorKitEvent(timestamp: date.timeIntervalInMilli, sensor: sensor.lampIdentifier, data: json)
         callbackdelegate?.onSensorFetch(model)
     }
     
